4e74bab25a0730f5cdb2a939881f3a90f05eea37
ds1242 <david.shaw1242@gmail.com>
2021-12-11 14:05:48 -0700

    setup cart to display with fake data

--- client/src/components/Cart/index.js
+++ client/src/components/Cart/index.js
@@ -6,25 +6,26 @@ import './style.css';
 
 const Cart = () => {
     return (
-        <div className='cart'>
-            <div className='close'>[close]
-            <h2>Shopping Cart</h2>
-                <CartItem item={{name:'Camera', image:'camera.jpg', price:5, purchaseQuantity:3}} />
-                <CartItem item={{name:'Soap', image:'soap.jpg', price:6, purchaseQuantity:4}} />
-
-                <div className="flex-row space-between">
-                    <strong>Total: $0</strong>
-                    {
-                        Auth.loggedIn() ?
-                            <button>
-                                Checkout
-                            </button>
-                        :
-                        <span>(log in to check out)</span>
-                    }
-                </div>
+      <div className="cart">
+        <div className="close">[close]</div>
+        <h2>Shopping Cart</h2>
+        <div>
+            <CartItem item={{name:'Camera', image:'camera.jpg', price:5, purchaseQuantity:3}} />
+            <CartItem item={{name:'Soap', image:'soap.jpg', price:6, purchaseQuantity:4}} />
+  
+            <div className="flex-row space-between">
+              <strong>Total: $0</strong>
+              {
+                Auth.loggedIn() ?
+                  <button>
+                    Checkout
+                  </button>
+                  :
+                  <span>(log in to check out)</span>
+              }
             </div>
-        </div>
+          </div>
+      </div>
     );
 };
 
